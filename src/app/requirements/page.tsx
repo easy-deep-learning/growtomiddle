c023e60e0b8fff3a22232a51fcf1d15344efc602
Alex Baumgertner <alex.baumgertner@gmail.com>
2023-10-31 19:48:50 +0300

    Add common requirements
    Copied from https://career-ladders.dev/engineering

---
+++ src/app/requirements/page.tsx
@@ -0,0 +1,19 @@
+const RequirementsPage = () => {
+  return (
+    <div>
+Completes clearly-defined tasks and communicates when something is blocking.
+Works to learn the relevant technologies and systems in place, actively maintains and extends the system.
+Communicates internally and externally in a respectful manner.
+Makes an impact by delivering features, refactoring existing code, or updating systems enable other developers to be successful.
+Helps debug technical problems. Submits issues so that we can document and improve our service.
+Works on engineering initiatives that drive a larger featureset, while being careful to not create technical debt in the process.
+Preemptively scans issue backlogs. Actions include but are not limited to: familiarizing themselves with pain points in the system, applying relevant labels and triaging, closing them via pull request or other means.
+Is considerate of others on the team and tries to support other people in their work.
+Works to become proficient in giving great reviews of their peers work. This includes being respectful, thorough, thoughtful, flexible, and not pedantic.
+Listens to the priorities and concerns of others, and acknowledges differing perspectives. Anticipates others’ reactions.
+    <a href="https://career-ladders.dev/engineering/#engineer-i">
+      https://career-ladders.dev/engineering
+    </a>
+</div>
+  )
+}
