875b6a328605cde6bebdc1a16617935a6027cbea
alexbaumgertner <alex.baumgertner@gmail.com>
2024-02-10 18:30:53 +0300

    Add Vercel analytics https://vercel.com/docs/analytics/quickstart

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import { Metadata } from 'next'
+import { Analytics } from '@vercel/analytics/react'
 import ApolloWrapper from '@/apollo/client-side-components'
 
 import { NextAuthProvider } from './providers'
 
 import '@/styles/global.css'
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'gymnasia',
   description: 'gymnasia — education world for all',
 }
@@ -23,6 +25,7 @@ export default function RootLayout ({
         {children}
       </ApolloWrapper>
     </NextAuthProvider>
+    <Analytics />
     </body>
     </html>
   )
