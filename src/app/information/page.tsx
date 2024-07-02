23b0bc1b1cd3193abc0c64bd55fe302d8bcd176a
alexbaumgertner <alex.baumgertner@gmail.com>
2024-07-02 16:54:20 +0100

    Restructured

---
+++ src/app/information/page.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+
+export default function InformationPage () {
+  return (
+    <div className="InformationPage">
+      <main>
+        <h2>What its the information?</h2>
+        <p>Information is like knowing something.</p>
+        <p>It&apos;s facts, details, or news about a person, thing, or event.</p>
+        <ul>
+          <li>Example: A phone number is information.</li>
+          <li>Example: A recipe is information.</li>
+          <li>Example: A news story is information.</li>
+        </ul>
+        <p>Basically, anything that tells you something is information!</p>
+      </main>
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
-import { AuthPanel } from '@/components/AuthPanel'
 import Link from 'next/link'
 
 export default function HomePage () {
   return (
     <div className="HomePage">
-      <AuthPanel />
       <menu>
         <ul>
-          <li><Link href="/projects">Projects</Link></li>
-          <li><Link href="/learn-english">learn-english</Link></li>
+          <li><Link href="/information">What is the information?</Link></li>
+          <li>Binary logic: why and how</li>
+          <li>Programming languages</li>
+          <li>JavaScript and its runtimes</li>
+          <li>Client-Server interactions</li>
+          <li>Server: how store and transfer data</li>
+          <li>Client: HTML/CSS, DOM and types of rendering</li>
         </ul>
       </menu>
     </div>
