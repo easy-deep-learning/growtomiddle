baf72daa64c9aee4b715062c206dc25aaf12a384
alexbaumgertner <alexbaumgertner@192.168.64.8>
2024-09-04 10:50:57 +0100

    Learn by creating an app: add sceleton of the content

--- src/app/static-html/page.tsx
+++ src/app/static-html/page.tsx
@@ -1,8 +1,47 @@
 export default function StaticHTMLPage() {
     return (
-      <div>
+      <div className="StaticHTMLPage">
         <h1>Welcome to the Static HTML Page</h1>
         <p>This is a simple Next.js page.</p>
+        <section>
+            <header>
+                <h2>What is HTML and CSS?</h2>
+            </header>
+            <article>
+                <p>
+                    HTML (HyperText Markup Language) is the standard markup language for documents
+                     designed to be displayed in a web browser.
+                </p>
+            </article>
+            <article>
+                <h3>What is CSS?</h3>
+                <p>
+                    CSS (Cascading Style Sheets) is a stylesheet language used to describe 
+                    the presentation of a document written in HTML.
+                </p>
+            </article>
+            <article>
+                <h3>
+                    How to create a simple HTML page?
+                </h3>
+                <p>
+                    To create a simple HTML page, you need to write HTML code.
+                </p>
+            </article>
+            <article>
+                <h3>
+                    How to make your page available for the world?
+                </h3>
+                <p>
+                    To make your page available for the world, you need to upload it to a web server.
+                </p>
+            </article>
+            <footer>
+                <p>
+                    This is a simple HTML page.
+                </p>
+            </footer>
+        </section>
       </div>
     )
   }
