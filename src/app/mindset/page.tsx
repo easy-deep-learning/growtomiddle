08f5a3384033f97c296cbebdfb8c42322cb3a651
alexbaumgertner <alex.baumgertner@gmail.com>
2024-09-05 14:34:31 +0300

    Fix code style and add a link to readmake

--- src/app/mindset/page.tsx
+++ src/app/mindset/page.tsx
@@ -1,40 +1,48 @@
-export default function MindsetPage() {
-    return (
-        <div className="MindsetPage">
-            <h1>Mindset</h1>
-            <section>
-                <header>
-                    <h2>What is mindset?</h2>
-                </header>
-                <article>
-                    
-                </article>
-            </section>
-            <section>
-                <header>
-                    <h2>Getting Real</h2>w
-                </header>
-                <article>
-                    <p>
-                    Want to build a successful web app? Then it’s time to Get Real. Getting Real is a smaller, faster, better way to build software.
+export default function MindsetPage () {
+  return (
+    <div className="MindsetPage">
+      <h1>Mindset</h1>
+      <section>
+        <header>
+          <h2>What is mindset?</h2>
+        </header>
+        <article>
 
-Getting Real is about skipping all the stuff that represents real (charts, graphs, boxes, arrows, schematics, wireframes, etc.) and actually building the real thing.
-Getting real is less. Less mass, less software, less features, less paperwork, less of everything that’s not essential (and most of what you think is essential actually isn’t).
-Getting Real is staying small and being agile.
-Getting Real starts with the interface, the real screens that people are going to use. It begins with what the customer actually experiences and builds backwards from there. This lets you get the interface right before you get the software wrong.
-Getting Real is about iterations and lowering the cost of change. Getting Real is all about launching, tweaking, and constantly improving which makes it a perfect approach for web-based software.
-Getting Real delivers just what customers need and eliminates anything they don’t.
+        </article>
+      </section>
+      <section>
+        <header>
+          <h2>Getting Real</h2>w
+        </header>
+        <article>
+          <p>
+            Want to build a successful web app? Then it’s time to Get Real. Getting Real is a smaller, faster, better way to build software.
 
-                    </p>
-                    <p>
-                    <a href="https://basecamp.com/gettingreal/" target="_blank">Getting Real</a>
-                    </p>
-                </article>
-            </section>
-            <section>   
-                <header><h2>The Interface</h2>
-                </header>
-            </section>
-        </div>
-    )
+            Getting Real is about skipping all the stuff that represents real (charts, graphs, boxes, arrows, schematics, wireframes, etc.) and actually
+            building the real thing.
+            Getting real is less. Less mass, less software, less features, less paperwork, less of everything that’s not essential (and most of what you think
+            is essential actually isn’t).
+            Getting Real is staying small and being agile.
+            Getting Real starts with the interface, the real screens that people are going to use. It begins with what the customer actually experiences and
+            builds backwards from there. This lets you get the interface right before you get the software wrong.
+            Getting Real is about iterations and lowering the cost of change. Getting Real is all about launching, tweaking, and constantly improving which
+            makes it a perfect approach for web-based software.
+            Getting Real delivers just what customers need and eliminates anything they don’t.
+
+          </p>
+          <p>
+            <a href="https://basecamp.com/gettingreal/" target="_blank">Getting Real</a>
+          </p>
+        </article>
+      </section>
+      <section>
+        <header>
+          <h2>Indie hacking</h2>
+          <p>
+            <a href="https://readmake.com">THE INDIE MAKER HANDBOOK</a>
+          </p>
+        </header>
+      </section>
+    </div>
+  )
 }
